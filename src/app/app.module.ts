c7189e09839c06137446568832d1c3ebf1d958e3
XavierRamos <xavier.ramos@cpavs.mx>
2020-05-18 08:58:28 -0500

    Update modules and code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CoreModule } from './core/core.module';
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
-import { NavComponent } from './shared/components/nav/nav.component';
-import { ResultsTableComponent } from './shared/components/results-table/results-table.component';
-import { CardComponent } from './shared/components/card/card.component';
-import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    ResultsTableComponent,
-    CardComponent,
-    DialogAddUserComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    CoreModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
